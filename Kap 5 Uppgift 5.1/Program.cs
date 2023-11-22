730d0b2d6fa74fdc87bd78ab286eec248e8fb4bc
humy06001 <humy06001@E-PW0234KS>
2023-11-22 20:15:15 +0100

    Add project files.

---
+++ Kap 5 Uppgift 5.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Färger
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Clear();
+
+            //-------------------------------------------
+
+            string[] sentances = { "Hej jag heter Hugo!", "Vad heter du?", "Jag äter banan.", "Banan är överskattat." };
+
+            //Utskrift
+            foreach (string sentance in sentances)
+            {
+                Console.WriteLine(sentance + "\n");
+            }
+        }
+    }
+}
+/*Skapa ett program som innehåller en array med strängar där varje sträng är en
+mening. Skriv ut varje mening med hjälp av en loop, gör så att varje mening hamnar i
+ett eget stycke, det ska alltså vara en tom rad mellan varje mening.*/
